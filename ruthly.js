82e3fba27b923e9cd4b0b481f2a12e6b3594f499
Davies Okpeta <davies19okpeta@gmail.com>
2024-02-01 09:35:30 +0100

    Update ruthly.js

--- ruthly.js
+++ ruthly.js
@@ -7,90 +7,87 @@ const errorImg = error
 const successImg = success
 
 
- const RuthdoAlert = (...props) => {
-
-   
-            const element1 = document.createElement('div')
-            const rootElement = document.getElementById('app')
-            rootElement.append(element1)
+ 
+const RuthdoAlert = (...props) => {
 
 
-            element1.setAttribute('id', 'alertMessage')
-            element1.setAttribute('style',
-                'width:362px;\n' +
-                'min-height: 78px;\n' +
-                'z-index: 999999999;\n' +
-                'position: fixed;\n' +
-                `background: ${props[0]?.icon === 'success' ? '#BBE7AC' : '#F89191'};\n` +
-                'border-radius: 8px;\n' +
-                'top: 100px;\n' +
-                'right: 50px;\n' +
-                'padding: 10px;\n' +
-                'display: flex;\n' +
-                'align-items: center;\n' +
-                'justify-content: center;\n' +
-                'transform-origin: 0 0;\n' +
-                'zoom: 75%;\n')
-            element1.setAttribute('class', 'animate__animated animate__fadeInRight')
-            const element2 = document.createElement('div')
-            element1.append(element2)
+    const element1 = document.createElement('div')
+    const rootElement = document.body
+    rootElement.append(element1)
+    const randomId = `alertMessage${Math.random()}`
 
-            element2.setAttribute('style',
-                'display: flex;\n ' +
-                'align-items: center;\n' +
-                'justify-content: space-around;\n' +
-                'width: 100%;\n' +
-                'height: inherit;\n')
+    element1.setAttribute('id', randomId)
+    element1.setAttribute('style',
+        'width:362px;\n' +
+        'min-height: 78px;\n' +
+        'z-index: 999999999;\n' +
+        'position: fixed;\n' +
+        `background: ${props[0]?.icon === 'success' ? '#BBE7AC' : '#F89191'};\n` +
+        'border-radius: 8px;\n' +
+        'top: 100px;\n' +
+        'right: 50px;\n' +
+        'padding: 10px;\n' +
+        'display: flex;\n' +
+        'align-items: center;\n' +
+        'justify-content: center;\n' +
+        'transform-origin: 0 0;\n' +
+        'zoom: 75%;\n')
+    element1.setAttribute('class', 'animate__animated animate__fadeInRight')
+    const element2 = document.createElement('div')
+    element1.append(element2)
 
-            const  elementP = document.createElement('p')
-            elementP.setAttribute('style',
-                'font-size: 18px;\n ' +
-                'line-height: 23px;\n' +
-                'letter-spacing: -0.01em;\n' +
-                `color: ${props[0]?.icon === 'success' ? '#415251' : '#FFFFFF'};\n` +
-                'width: 70%;\n' +
-                'font-family: \'IBM Plex Sans\';\n' +
-                'font-style: normal;')
+    element2.setAttribute('style',
+        'display: flex;\n ' +
+        'align-items: center;\n' +
+        'justify-content: space-around;\n' +
+        'width: 100%;\n' +
+        'height: inherit;\n')
 
-            elementP.textContent = props[0]?.title
+    const elementP = document.createElement('p')
+    elementP.setAttribute('style',
+        'font-size: 18px;\n ' +
+        'line-height: 23px;\n' +
+        'letter-spacing: -0.01em;\n' +
+        `color: ${props[0]?.icon === 'success' ? '#415251' : '#FFFFFF'};\n` +
+        'width: 70%;\n' +
+        'font-family: \'IBM Plex Sans\';\n' +
+        'font-style: normal;')
 
-            const elementClose = document.createElement('img')
-            elementClose.setAttribute('src', cancelImg)
-            elementClose.setAttribute('style', 'cursor:pointer')
+    elementP.textContent = props[0]?.title
 
-            const elementType = document.createElement('img')
-            elementType.setAttribute( 'src', props[0]?.icon === 'success' ? successImg : props[0]?.icon === 'error' ? errorImg : null)
-            element2.append(elementType)
-            element2.append(elementP)
-            element2.append(elementClose)
+    const elementClose = document.createElement('img')
+    elementClose.setAttribute('src', cancelImg)
+    elementClose.setAttribute('style', 'cursor:pointer')
 
-            const removeElement = document.getElementById('alertMessage')
+    const elementType = document.createElement('img')
+    elementType.setAttribute('src', props[0]?.icon === 'success' ? successImg : props[0]?.icon === 'error' ? errorImg : null)
+    element2.append(elementType)
+    element2.append(elementP)
+    element2.append(elementClose)
 
-            const timeOut = props[0]?.timeout // auto modal dismissal timeout
+    const removeElement = document.getElementById(randomId)
 
-            // on click modal dismissal events
-            elementClose.addEventListener('click', () => {
-                removeElement.setAttribute('class', 'animate__animated animate__fadeOutRight')
-                setTimeout(() => {
-                removeElement.remove()
-            },1000)
-            })
+    const timeOut = props[0]?.timeout // auto modal dismissal timeout
 
-
-            function autoDismissal(){
-                removeElement.setAttribute('class', 'animate__animated animate__fadeOutRight')
-                setTimeout(() => {
-                    removeElement.remove()
-                },1000)
-            }
-
-            setTimeout(() => {
-                autoDismissal()
-            },timeOut ? timeOut : 3000)
+    // on click modal dismissal events
+    elementClose.addEventListener('click', () => {
+        removeElement.setAttribute('class', 'animate__animated animate__fadeOutRight')
+        setTimeout(() => {
+            removeElement.remove()
+        }, 1000)
+    })
 
 
-        
+    function autoDismissal() {
+        removeElement.setAttribute('class', 'animate__animated animate__fadeOutRight')
+        setTimeout(() => {
+            removeElement.remove()
+        }, 1000)
+    }
 
+    setTimeout(() => {
+        autoDismissal()
+    }, timeOut ? timeOut : 3000)
 }
 
 export default RuthdoAlert
