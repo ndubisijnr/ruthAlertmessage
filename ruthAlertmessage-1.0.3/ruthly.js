cfb674e51763c172bf35a92b3cdcb636d9a44fe3
ndubisijnr <ndubisijnr@gmail.com>
2024-02-28 11:42:23 +0100

    made update

--- ruthAlertmessage-1.0.3/ruthly.js
+++ ruthAlertmessage-1.0.3/ruthly.js
@@ -9,142 +9,150 @@ const successImg = success
 let ruthItemArray = []
 
 
-// const ruthToastProps = {
-//     title,
-//     message,
-//     timeout,
-//     icon 
-// }
-
-//  export const RuthdoAlert = (...props) => {
-
-//             const element1 = document.createElement('div')
-//             const rootElement = document.getElementById('app')
-//             rootElement.append(element1)
-
- 
-//             const timeOut = props[0]?.timeout // auto modal dismissal timeout    
-    
-//             element1.setAttribute('id', 'alertMessage')
-//             element1.setAttribute('style',
-//                 'width:362px;\n' +
-//                 'min-height: 78px;\n' +
-//                 'z-index: 999999999;\n' +
-//                 'position: fixed;\n' +
-//                 `background: ${props[0]?.icon === 'success' ? '#BBE7AC' : '#F89191'};\n` +
-//                 'border-radius: 8px;\n' +
-//                 'top: 100px;\n' +
-//                 'right: 50px;\n' +
-//                 'padding: 10px;\n' +
-//                 'display: flex;\n' +
-//                 'align-items: center;\n' +
-//                 'justify-content: center;\n' +
-//                 'transform-origin: 0 0;\n' +
-//                 'zoom: 75%;\n')
-//             element1.setAttribute('class', 'animate__animated animate__fadeInRight')
-//             const element2 = document.createElement('div')
-//             element1.append(element2)
-    
-//             element2.setAttribute('style',
-//                 'display: flex;\n ' +
-//                 'align-items: center;\n' +
-//                 'justify-content: space-around;\n' +
-//                 'width: 100%;\n' +
-//                 'height: inherit;\n')
-    
-//             const  elementP = document.createElement('p')
-//             elementP.setAttribute('style',
-//                 'font-size: 18px;\n ' +
-//                 'line-height: 23px;\n' +
-//                 'letter-spacing: -0.01em;\n' +
-//                 `color: ${props[0]?.icon === 'success' ? '#415251' : '#FFFFFF'};\n` +
-//                 'width: 70%;\n' +
-//                 'font-family: \'IBM Plex Sans\';\n' +
-//                 'font-style: normal;')
-    
-//             elementP.textContent = props[0]?.title
-    
-//             const elementClose = document.createElement('img')
-//             elementClose.setAttribute('src', cancelImg)
-//             elementClose.setAttribute('style', 'cursor:pointer')
-//             elementClose.setAttribute('id', 'close_ruthly')
-    
-    
-//             const elementType = document.createElement('img')
-//             elementType.setAttribute( 'src', props[0]?.icon === 'success' ? successImg : props[0]?.icon === 'error' ? errorImg : null)
-//             element2.append(elementType)
-//             element2.append(elementP)
-//             element2.append(elementClose)
-
-//             const removeElement = document.getElementById('alertMessage')
-
-
-//                 // on click modal dismissal events
-//                 function clickToClose(){
-//                     removeElement.setAttribute('class', 'animate__animated animate__fadeOutRight')
-//                         setTimeout(() => {
-//                         removeElement.remove()
-//                     },1000)
-//                 }
-
-//                 document.getElementById('close_ruthly').addEventListener('click', (e) => {
-//                     console.log(e, removeElement, boxArray)
-//                     clickToClose()
-//                     console.log(e, removeElement, boxArray)
-//                 })
-
-//                 function autoDismissal(){
-//                     removeElement.setAttribute('class', 'animate__animated animate__fadeOutRight')
-//                     setTimeout(() => {
-//                         removeElement.remove()
-//                     },1000)
-//                 }
-
-//                 setTimeout(() => {
-//                     autoDismissal()
-//                 },timeOut ? timeOut : 3000)
-        
-             
-    
-
-//             if(boxArray.length > 0){
-//                 autoDismissal()
-//                 boxArray = []
-//                 return; 
-//             }else{
-//                 boxArray.push({})
-//                 console.log('push', boxArray)
-//                 addToBoxArray()
-//             }
-
-
-
-       
-
-
-        
-
-// }
-
-
 //point of entry
+// export const RuthdoAlert = (...ruthToastProps) => {
+//     const props = ruthToastProps[0]
+
+
+//     const alert = (el, ruthItems) => {
+
+//         ruthItems.forEach((element,index) => {
+//         console.log(element)
+
+//         el.setAttribute('id', `${index}-alertMessage`)
+
+//         el.setAttribute('style',
+//             'width:362px;\n' +
+//             'min-height: 78px;\n' +
+//             'z-index: 999999;\n' +
+//             'position: relative;\n' +
+//             `background: ${props[0]?.icon === 'success' ? '#BBE7AC' : '#F89191'};\n` +
+//             'border-radius: 8px;\n' +
+//             'top: 0;\n' +
+//             'right: 0;\n' +
+//             'padding: 10px;\n' +
+//             'display: flex;\n' +
+//             'align-items: center;\n' +
+//             'justify-content: center;\n' +
+//             'transform-origin: 0 0;\n' +
+//             'zoom: 75%;\n' +
+//             'margin-bottom:15px;')
+//         el.setAttribute('class', 'animate__animated animate__fadeInRight')
+//         const element2 = document.createElement('div')
+//         el.appendChild(element2)
+    
+//         element2.setAttribute('style',
+//             'display: flex;\n ' +
+//             'align-items: center;\n' +
+//             'justify-content: space-around;\n' +
+//             'width: 100%;\n' +
+//             'height: inherit;\n')
+    
+//         const  elementP = document.createElement('p')
+//         elementP.setAttribute('style',
+//             'font-size: 18px;\n ' +
+//             'line-height: 23px;\n' +
+//             'letter-spacing: -0.01em;\n' +
+//             `color: ${props?.icon === 'success' ? '#415251' : '#FFFFFF'};\n` +
+//             'width: 70%;\n' +
+//             'font-family: \'IBM Plex Sans\';\n' +
+//             'font-style: normal;')
+    
+//         elementP.textContent = props?.title
+    
+//         const elementClose = document.createElement('img')
+//         elementClose.setAttribute('src', cancelImg)
+//         elementClose.setAttribute('style', 'cursor:pointer')
+//         elementClose.setAttribute('id', `${index}-close_ruthly`)
+    
+    
+//         const elementType = document.createElement('img')
+//         elementType.setAttribute( 'src', props?.icon === 'success' ? successImg : props?.icon === 'error' ? errorImg : null)
+//         element2.append(elementType)
+//         element2.append(elementP)
+//         element2.append(elementClose) 
+        
+//         const removeElement = document.getElementById(`${index}-alertMessage`)
+
+
+//         // on click modal dismissal events
+//         function clickToClose(){
+//             removeElement.setAttribute('class', 'animate__animated animate__fadeOutRight')
+//                 setTimeout(() => {
+//                 removeElement.remove()
+//             },1000)
+//         }
+
+//          document.getElementById(`${index}-close_ruthly`).addEventListener('click', (e) => {
+//             console.log(e, removeElement, boxArray)
+//             clickToClose()
+//             console.log(e, removeElement, boxArray)
+//         })
+        
+        
+//         function autoDismissal(){
+//             removeElement.setAttribute('class', 'animate__animated animate__fadeOutRight')
+//             setTimeout(() => {
+//                 removeElement.remove()
+//             },1000)
+//         }
+    
+//         setTimeout(() => {
+//             autoDismissal()
+//         },props.timeout ? props.timeout : 3000)
+        
+        
+//        });
+
+//        return el
+        
+//     }
+
+//     const parent = document.getElementById('app')
+//     const ruthlyChildcoverDiv = document.createElement('div')
+//     parent.append(ruthlyChildcoverDiv)
+//     ruthlyChildcoverDiv.setAttribute('style', 'width:362px;\n' +
+//     'z-index: 999999999;\n' +
+//     'position: fixed;\n' +
+//     'top: 100px;\n' +
+//     'right: 50px;\n' +
+//     'display: flex;\n' +
+//     'align-items: center;\n' +
+//     'justify-content: center;\n')
+//     // Create main element
+//     const element1 = document.createElement('div') 
+//     ruthItemArray.push(element1)
+//     ruthlyChildcoverDiv.appendChild(alert(element1, ruthItemArray))
+// }
+
+
 export const RuthdoAlert = (...ruthToastProps) => {
-    const props = ruthToastProps[0]
+    const props = ruthToastProps[0];
+    const parent = document.getElementById('app');
+    const ruthlyChildcoverDiv = document.createElement('div');
+    parent.append(ruthlyChildcoverDiv);
+    ruthlyChildcoverDiv.setAttribute('style', 'width:362px;\n' +
+        'z-index: 999999999;\n' +
+        'position: fixed;\n' +
+        'top: 100px;\n' +
+        'right: 50px;\n' +
+        'display: flex;\n' +
+        'align-items: center;\n' +
+        'justify-content: center;\n');
 
+    // Create main element
+    const element1 = document.createElement('div');
+    ruthlyChildcoverDiv.appendChild(element1);
 
-    const alert = (el, ruthItems) => {
-
-        ruthItems.forEach((element,index) => {
-        console.log(element)
-
-        el.setAttribute('id', `${index}-alertMessage`)
-
+    // Function to create individual alert elements
+    const createAlert = (el, props, index) => {
+        el.setAttribute('id', `${index}-alertMessage`);
         el.setAttribute('style',
             'width:362px;\n' +
             'min-height: 78px;\n' +
             'z-index: 999999;\n' +
             'position: relative;\n' +
-            `background: ${props[0]?.icon === 'success' ? '#BBE7AC' : '#F89191'};\n` +
+            `background: ${props?.icon === 'success' ? '#BBE7AC' : '#F89191'};\n` +
             'border-radius: 8px;\n' +
             'top: 0;\n' +
             'right: 0;\n' +
@@ -154,19 +162,20 @@ export const RuthdoAlert = (...ruthToastProps) => {
             'justify-content: center;\n' +
             'transform-origin: 0 0;\n' +
             'zoom: 75%;\n' +
-            'margin-bottom:15px;')
-        el.setAttribute('class', 'animate__animated animate__fadeInRight')
-        const element2 = document.createElement('div')
-        el.appendChild(element2)
-    
+            'margin-bottom:15px;');
+        el.setAttribute('class', 'animate__animated animate__fadeInRight');
+
+        const element2 = document.createElement('div');
+        el.appendChild(element2);
+
         element2.setAttribute('style',
             'display: flex;\n ' +
             'align-items: center;\n' +
             'justify-content: space-around;\n' +
             'width: 100%;\n' +
-            'height: inherit;\n')
-    
-        const  elementP = document.createElement('p')
+            'height: inherit;\n');
+
+        const elementP = document.createElement('p');
         elementP.setAttribute('style',
             'font-size: 18px;\n ' +
             'line-height: 23px;\n' +
@@ -174,71 +183,48 @@ export const RuthdoAlert = (...ruthToastProps) => {
             `color: ${props?.icon === 'success' ? '#415251' : '#FFFFFF'};\n` +
             'width: 70%;\n' +
             'font-family: \'IBM Plex Sans\';\n' +
-            'font-style: normal;')
-    
-        elementP.textContent = props?.title
-    
-        const elementClose = document.createElement('img')
-        elementClose.setAttribute('src', cancelImg)
-        elementClose.setAttribute('style', 'cursor:pointer')
-        elementClose.setAttribute('id', `${index}-close_ruthly`)
-    
-    
-        const elementType = document.createElement('img')
-        elementType.setAttribute( 'src', props?.icon === 'success' ? successImg : props?.icon === 'error' ? errorImg : null)
-        element2.append(elementType)
-        element2.append(elementP)
-        element2.append(elementClose) 
-        
-        const removeElement = document.getElementById(`${index}-alertMessage`)
+            'font-style: normal;');
+        elementP.textContent = props?.title;
 
+        const elementClose = document.createElement('img');
+        elementClose.setAttribute('src', cancelImg);
+        elementClose.setAttribute('style', 'cursor:pointer');
+        elementClose.setAttribute('id', `${index}-close_ruthly`);
 
-        // on click modal dismissal events
-        function clickToClose(){
-            removeElement.setAttribute('class', 'animate__animated animate__fadeOutRight')
-                setTimeout(() => {
-                removeElement.remove()
-            },1000)
-        }
+        const elementType = document.createElement('img');
+        elementType.setAttribute('src', props?.icon === 'success' ? successImg : props?.icon === 'error' ? errorImg : null);
+        element2.append(elementType);
+        element2.append(elementP);
+        element2.append(elementClose);
 
-         document.getElementById(`${index}-close_ruthly`).addEventListener('click', (e) => {
-            console.log(e, removeElement, boxArray)
-            clickToClose()
-            console.log(e, removeElement, boxArray)
-        })
-        
-        
-        function autoDismissal(){
-            removeElement.setAttribute('class', 'animate__animated animate__fadeOutRight')
+        const removeElement = document.getElementById(`${index}-alertMessage`);
+
+        // Function to close the alert
+        function clickToClose() {
+            removeElement.setAttribute('class', 'animate__animated animate__fadeOutRight');
             setTimeout(() => {
-                removeElement.remove()
-            },1000)
+                removeElement.remove();
+            }, 1000);
         }
-    
-        setTimeout(() => {
-            autoDismissal()
-        },props.timeout ? props.timeout : 3000)
-        
-        
-       });
 
-       return el
-        
-    }
+        // Event listener to close the alert on click
+        document.getElementById(`${index}-close_ruthly`).addEventListener('click', (e) => {
+            clickToClose();
+        });
 
-    const parent = document.getElementById('app')
-    const ruthlyChildcoverDiv = document.createElement('div')
-    parent.append(ruthlyChildcoverDiv)
-    ruthlyChildcoverDiv.setAttribute('style', 'width:362px;\n' +
-    'z-index: 999999999;\n' +
-    'position: fixed;\n' +
-    'top: 100px;\n' +
-    'right: 50px;\n' +
-    'display: flex;\n' +
-    'align-items: center;\n' +
-    'justify-content: center;\n')
-    // Create main element
-    const element1 = document.createElement('div') 
-    ruthItemArray.push(element1)
-    ruthlyChildcoverDiv.appendChild(alert(element1, ruthItemArray))
-}
+        // Function to dismiss the alert after a timeout
+        function autoDismissal() {
+            removeElement.setAttribute('class', 'animate__animated animate__fadeOutRight');
+            setTimeout(() => {
+                removeElement.remove();
+            }, props.timeout ? props.timeout : 3000);
+        }
+
+        autoDismissal(); // Initiate auto-dismissal
+    };
+
+    // Loop through ruthToastProps and create alert elements
+    ruthToastProps.forEach((props, index) => {
+        createAlert(element1.cloneNode(true), props, index);
+    });
+};
